bd09e8990dde973996d0ede02452d7f25e63372d
yuyu0127 <saiyuki01271210@gmail.com>
2021-04-19 00:58:05 +0900

    debug goal

--- job.py
+++ job.py
@@ -6,6 +6,7 @@ __import__('dotenv').load_dotenv()
 WEBHOOK_URL = os.environ.get('WEBHOOK_URL')
 
 for goal in db.fetch_goals():
+    print(goal)
     try:
         slackapi.post_direct_message(goal['user_id'], text=goal['content'])
     except Exception as e:
